6e18b8d83ea91f594e27c01b06fd7c2a15d3b953
Tyson Yen <tysonyen@Tysons-Mac-mini.local>
2015-01-15 18:36:16 -0800

    Initial Commit

---
+++ Postcard 3Tests/Postcard_3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Postcard_3Tests.swift
+//  Postcard 3Tests
+//
+//  Created by Tyson Yen on 1/15/15.
+//  Copyright (c) 2015 Tyson Yen. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Postcard_3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
